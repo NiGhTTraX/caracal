762a29ea1deb8c206c6fe80349d4dacaa3c7f7ae
Andrei Picus <office.nightcrawler@gmail.com>
2013-12-06 20:21:33 +0200

    Remove other math functions.

--- eve.py
+++ eve.py
@@ -6,7 +6,6 @@ import numpy as np
 
 # Hyperbolic secant function.
 sech = lambda x: 2 / (np.exp(x) + np.exp(-x))
-#sech = lambda x: 2 * np.cosh(x) / (np.cosh(2 * x) + 1)
 
 # Inverse hyperbolic secant function.
 arcsech = lambda x: np.arccosh(1 / x)
@@ -68,9 +67,3 @@ class CapModelLinear(CapModelBase):
   def recharge(self, delta):
     self.current += self._cap / self._recharge * delta
 
-
-cfunc = lambda T, C, t: C * (1 - 1 / (np.cosh(5/T * t)))
-ifunc = lambda T, C, c: 1 / (5/T) * arcsech((C - c) / C)
-rfunc = lambda T, C, t: -C * sech(5/T * t)
-rfunc2 = lambda T, C, t: -2 * C * np.cosh(5/T * t) / (np.cosh(2 * (5/T) * t) + 1)
-rfunc3 = lambda T, c, C, d: C * (1 + (sqrt(c / C) - 1) * np.exp(-d * (5/T)))**2
